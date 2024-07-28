699393b33d70eea3d3e9bd6c152b2c7adf83d2af
DerekG29 <derekgrubbs29@gmail.com>
2024-07-28 13:40:33 -0400

    auth-router completed

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,42 +1,47 @@
 const router = require("express").Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { checkUsernameExists, validateRoleName } = require('./auth-middleware');
 const { JWT_SECRET } = require("../secrets"); // use this secret!
 
+const {
+  add,
+} = require('../users/users-model')
+
 router.post("/register", validateRoleName, (req, res, next) => {
-  /**
-    [POST] /api/auth/register { "username": "anna", "password": "1234", "role_name": "angel" }
+  const { username, password } = req.body;
+  const role_name = req.role_name;
 
-    response:
-    status 201
-    {
-      "user"_id: 3,
-      "username": "anna",
-      "role_name": "angel"
-    }
-   */
+  const hash = bcrypt.hashSync(password, 8)
+
+  add({ username, password: hash, role_name })
+    .then(([saved]) => {
+      res.status(201).json(saved);
+    })
+    .catch(next)
 });
-
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
-  /**
-    [POST] /api/auth/login { "username": "sue", "password": "1234" }
+  const { username, password, user } = req.body;
 
-    response:
-    status 200
-    {
-      "message": "sue is back!",
-      "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ETC.ETC"
-    }
-
-    The token must expire in one day, and must provide the following information
-    in its payload:
-
-    {
-      "subject"  : 1       // the user_id of the authenticated user
-      "username" : "bob"   // the username of the authenticated user
-      "role_name": "admin" // the role of the authenticated user
-    }
-   */
+  if (bcrypt.compareSync(password, user.password)) {
+    const token = buildToken(user)
+    res.json({ message: `${username} is back!`, token });
+  } else {
+    next({ status: 401, message: 'Invalid credentials' });
+  }
 });
+
+function buildToken(user) {
+  const payload = {
+    subject: user.user_id,
+    username: user.username,
+    role_name: user.role_name
+  }
+  const options = {
+    expiresIn: '1d'
+  }
+  return jwt.sign(payload, JWT_SECRET, options)
+}
 
 module.exports = router;
